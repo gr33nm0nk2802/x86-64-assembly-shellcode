805325f9c2bd20b0b333cd778782801cbdb972a7
gr33nm0nk2802 <gr33nm0nk2802@gmail.com>
2020-12-31 19:53:21 +0530

    Stack in assembly

---
+++ Assembly/code/4.Stack/stack.asm
@@ -0,0 +1,39 @@
+; Author: d4rk-c1ph3r
+; A program to demonstrate the Working of a stack
+; The function prolouge and function epilouge
+
+; Assembling: nasm -f elf32 -o stack.o stack.asm
+; linking:    ld -m elf_i386 -o stack stack.o
+
+	section .data
+msg1:    db "This is the outer function",0xa
+msg1len: equ $-msg1
+msg2:    db "This is inner function",0xa
+msg2len: equ $-msg2
+
+	section .text
+	global _start
+_start:
+	; outer function
+	mov eax, 0x4
+	mov ebx, 0x1
+	mov ecx, msg1
+	mov edx, msg1len
+	int 0x80
+
+	call fun
+	mov eax, 0x1
+	mov ebx, 0x0
+	int 0x80
+
+fun:
+	; function prolouge
+	push ebp
+	mov ebp, esp
+	mov eax, 0x4
+	mov ebx, 0x1
+	mov ecx, msg2
+	mov edx, msg2len
+	int 0x80
+	leave
+	ret
